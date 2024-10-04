c6011331055143d125a1f1d30283644846c0147b
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-04 23:55:26 +0300

    sequences

--- Helloworld.c
+++ Helloworld.c
@@ -2,8 +2,9 @@
 int main(){
     // This is a comment blah
     /*This is a comment*/
-    printf("I like pizza\n");
-    printf("It's really good\n");
-    printf("1\t2\t3\n4\t5\t6\n7\t8");
+    //printf("I like pizza\n");
+    //printf("It's really good\n");
+    //printf("1\t2\t3\n4\t5\t6\n7\t8");
+    printf("\"I like pizza\" - Abraham Lincoln probably");
     return 0;
 }
