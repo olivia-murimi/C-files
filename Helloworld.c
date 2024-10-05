8358a02d1cb9652a67fa485e7cfdfbbf717f0fb1
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-05 23:42:32 +0300

    variables

--- Helloworld.c
+++ Helloworld.c
@@ -5,6 +5,7 @@ int main(){
     //printf("I like pizza\n");
     //printf("It's really good\n");
     //printf("1\t2\t3\n4\t5\t6\n7\t8");
-    printf("\"I like pizza\" - Abraham Lincoln probably");
+    //printf("\"I like pizza\" - Abraham Lincoln probably");
+    int x;// declaration
     return 0;
 }
