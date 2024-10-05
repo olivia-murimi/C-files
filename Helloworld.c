08ef627539cfe72ba27c6a1cca2c41b83af46899
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-05 23:46:20 +0300

    variables

--- Helloworld.c
+++ Helloworld.c
@@ -8,6 +8,9 @@ int main(){
     //printf("\"I like pizza\" - Abraham Lincoln probably");
     int x;// declaration
     x = 123; //initialization
-    int y =
+    int y = 321; //declaration + initialization
+    int age = 21;
+    float gpa = 2.05; //floating point number containg a decimal
+    char grade = 'C';//single characters
     return 0;
 }
