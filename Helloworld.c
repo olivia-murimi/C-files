f1015fbcce8f6bb118c77e1f0686d0726acff91d
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-17 23:45:21 +0300

    area of a triangle

--- Helloworld.c
+++ Helloworld.c
@@ -116,11 +116,14 @@
 
       printf("Enter side A:");
       scanf("%lf", &A);
-
+      printf("Enter side B:");
+      scanf("%lf", &B);
+      
+      C = sqrt(A*A + B*B);
+      printf("Side C: %lf",C);
         return 0;
      }
 
 
 
     
-
