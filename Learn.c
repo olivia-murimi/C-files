097c3103a430844542e8d9bbadd371478907fdbb
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-19 22:42:07 +0300

    switch statement

--- Learn.c
+++ Learn.c
@@ -2,29 +2,18 @@
 #include <stdio.h>
 int main(){
 
-  char grade;
+  char grade ;
   printf("\nEnter a letter grade");
   scanf("%c",&grade);
-
-  if(grade == 'A'){
-    printf("perfect\n");
+  
+  switch(grade){
+    case 'A';
+      printf("perfect!\n");
+      break;
+      case 'B';
+      printf("You did goog!\n");
+      break;
   }
-  else if(grade =='B'){
-    printf("You did good\n");
-  }
-  else if(grade =='C'){
-    printf("You did okay\n");
-  }
-  else if(grade =='D'){
-    printf("Atleast it's not an F\n");
-  }
-  else if(grade =='F'){
-    printf("YOU FAILED\n");
-  }
-  else{
-    printf("That's not a valid grade\n");
-  } 
-
 
 
     return 0;
