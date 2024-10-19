3f82159d26863acf2eb696774342b4049c48091e
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-19 22:24:26 +0300

    switch statements

--- Learn.c
+++ Learn.c
@@ -1,1 +1,13 @@
-Switch A more efficient alternative to using many "else if" statements allows a value to be tested for equality against many cases.
+//switch A more efficient alternative to using many "else if" statements allows a value to be tested for equality against many cases.
+#include <stdio.h>
+int main(){
+
+  char grade;
+  printf("\nEnter a letter grade");
+  scanf("%c",&grade);
+
+
+
+
+    return 0;
+}
