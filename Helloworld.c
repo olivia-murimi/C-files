bac9f21c0275c66425f9a285189f42d4916b7fab
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-21 23:04:50 +0300

    OR

--- Helloworld.c
+++ Helloworld.c
@@ -147,14 +147,14 @@
  int main(){
    
    //logical operators = && (AND)checks if two conditions are true
-   logical operators = (OR) checks if at least one condition is true
+  // logical operators = (OR) checks if at least one condition is true
   float temp = 1000;
   bool sunny = false;
-  if (temp >= 0 && temp <= 30 && sunny){
-    printf("The weather is good");
+  if (temp <= 0){
+    printf("\nThe weather is good");
   }
   else{
-    printf("The weather is bad");
+    printf("\nThe weather is bad");
   }
 
    return 0;
