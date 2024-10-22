b23254f688796f2a80fa4a6e20cf03a3db9afdfb
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-22 23:23:17 +0300

    for loop

--- Helloworld.c
+++ Helloworld.c
@@ -185,10 +185,10 @@
  }*/
 #include <stdio.h>
 int main(){
-  for loop = repeats a section of code a limited amount of times
+  //for loop = repeats a section of code a limited amount of times
   for(int i= 1; i <=10; i++)
   {
-  
+    printf("test\n");
   }
    return 0;
 }
