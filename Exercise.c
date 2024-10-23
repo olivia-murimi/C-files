bebfe461fb6d69c140ac60a0e49ebe781df06339
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-23 23:37:19 +0300

    while loop

--- Exercise.c
+++ Exercise.c
@@ -16,6 +16,15 @@ int main(){
     printf("/nWhat's your name?:");
     fgets(name, 25, stdin);
     name[strlen(name) - 1] = '\0';
+    while(strlen(name) == 0)
+    {
+      printf("\nYou did not enter your name");
+      printf("/nWhat's your name?:");
+    fgets(name, 25, stdin);
+    name[strlen(name) - 1] = '\0';
+    while(strlen(name) == 0)
+    }
+    printf("Hello %s", name);
 
     return 0;
 }
