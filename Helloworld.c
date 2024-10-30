6586071c093c0507b55645161c16bbbc402f41df
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-30 22:29:34 +0300

    arrays

--- Helloworld.c
+++ Helloworld.c
@@ -211,7 +211,9 @@ int main()
 #include <stdio.h>
 int main (){
   //Array = a data structure that can store many values of the same data type
-  double price = 5.0;
+  double prices[]= {5.0, 10.0, 15.0, 25.0,  20.0};
+  printf("$%.2lf",prices[0]);
+
   return 0;
 }
 
