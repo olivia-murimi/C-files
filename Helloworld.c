ada7213f1080e42dd0d5c77a568a28f0a808dc76
Olivia Waithira <oliviawaithira7@gmail.com>
2024-10-30 22:24:36 +0300

    arrays

--- Helloworld.c
+++ Helloworld.c
@@ -210,7 +210,9 @@ int main()
 
 #include <stdio.h>
 int main (){
-  Array = a data structure that can store many values of the same data type
+  //Array = a data structure that can store many values of the same data type
+  double price = 5.0;
+  return 0;
 }
 
     
