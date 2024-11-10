408c5c5bbdfa97677b38d7b44a5b23d07ca25575
Olivia Waithira <oliviawaithira7@gmail.com>
2024-11-10 23:39:37 +0300

    structures

--- Helloworld.c
+++ Helloworld.c
@@ -345,6 +345,15 @@ int main(){
   struct player1;
   struct player2;
   srcpy(player1.name, "Bro");
+  player1.score = 4;
+   srcpy(player2.name, "Bra");
+  player1.score = 5;
+ 
+ printf("%s\n", player1.name);
+ printf("%d\n", player1.score);
+ 
+ printf("%s\n", player2.name);
+ printf("%d\n", player2.score);
 
   return 0;
 }
