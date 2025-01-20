8eb25faaf63d4e1fb795443a15fc16c47d9187a3
Olivia Waithira <oliviawaithira7@gmail.com>
2025-01-20 15:19:45 +0300

    adding a quote

--- C.c
+++ C.c
@@ -3,6 +3,7 @@
 int main(){
    printf("Hello world!\n"); 
    /*printf("This is a multiline comment\n"); */
-   printf("1\t2\t3");
+   printf("1\t2\t3\n");
+   printf("\"I like pizza\" - Abraham lincoln probably");
     return 0;
 }
