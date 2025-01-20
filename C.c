51b8af95be203f3c2823d03c806afd4885766fac
Olivia Waithira <oliviawaithira7@gmail.com>
2025-01-20 15:45:02 +0300

    variables

--- C.c
+++ C.c
@@ -1,9 +1,20 @@
-#include<stdio.h>
+/*#include<stdio.h>
 
 int main(){
    printf("Hello world!\n"); 
-   /*printf("This is a multiline comment\n"); */
+   printf("This is a multiline comment\n"); 
    printf("1\t2\t3\n");
    printf("\"I like pizza\" - Abraham lincoln probably");
+    return 0;
+}*/
+#include <stdio.h>
+int main(){
+    //variable = allocated space in a memory to store value.
+    int age = 21;
+    float gpa = 2.05;
+    char grade = 'B';
+    char name[] = "Olivia";
+
+    printf("I'm turning %d next year",age);
     return 0;
 }
